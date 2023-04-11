d76441dda74d804daec7634641a1881c3c0e8ad6
Fábio Souza <fhssouza@gmail.com>
2023-04-10 21:45:11 -0300

    project created

---
+++ src/main/java/br/com/apipessoa/ApiPessoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.apipessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPessoaApplication.class, args);
+	}
+
+}
