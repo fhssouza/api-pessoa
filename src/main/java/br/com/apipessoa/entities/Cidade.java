54b0ca02d0c984074fbafa3d3bcfacadebe566d0
Fábio Souza <fhssouza@gmail.com>
2023-04-11 13:23:51 -0300

    Project Lombok

--- src/main/java/br/com/apipessoa/entities/Cidade.java
+++ src/main/java/br/com/apipessoa/entities/Cidade.java
@@ -1,8 +1,8 @@
 package br.com.apipessoa.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,69 +11,25 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
-@Table(name = "tb_city")
-public class City implements Serializable {
+@Data
+@Table(name = "tb_cidade")
+public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	
+	@Column(length = 50, nullable = false)
+	private String nome;
 
 	@ManyToOne
-	@JoinColumn(name = "state_id")
-	private State state;
+	@JoinColumn(name = "estado_id")
+	private Estado estado;
 
-	public City() {
-	}
-
-	public City(Integer id, String name, State state) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.state = state;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public State getState() {
-		return state;
-	}
-
-	public void setState(State state) {
-		this.state = state;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		City other = (City) obj;
-		return Objects.equals(id, other.id);
-	}
+	
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_state (name) VALUES ('Pará');
-INSERT INTO tb_state (name) VALUES ('Amazonas');
+INSERT INTO tb_estado (nome) VALUES ('Pará');
+INSERT INTO tb_estado (nome) VALUES ('Amazonas');
 
-INSERT INTO tb_city (name, state_id) VALUES ('Belém', 1);
-INSERT INTO tb_city (name, state_id) VALUES ('Manaus', 2);
+INSERT INTO tb_cidade (nome, estado_id) VALUES ('Belém', 1);
+INSERT INTO tb_cidade (nome, estado_id) VALUES ('Manaus', 2);
